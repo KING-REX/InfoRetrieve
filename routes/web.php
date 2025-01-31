d422a4e8576f9537d385060624d26f58de9162f2
King Shadow <anijimjamesmaurice@gmail.com>
2025-01-31 23:39:25 +0100

    Changing the route and making it return relevant information in JSON format

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get("/", function () {
+    return response()
+        ->json(['email' => 'anijimjamesmaurice@gmail.com', 'current_datetime' => date('c'), 'github_url' => 'https://github.com/KING-REX/InfoRetrieve']);
 });
